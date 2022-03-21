9a59a474009a7ab874d5fafa099fb93d7a658c80
Limor <limor.mekaiten@gmail.com>
2022-03-21 12:02:57 +0200

    Update items.js

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -53,6 +53,9 @@ router.get("/", auth.optional, function(req, res, next) {
     query.tagList = { $in: [req.query.tag] };
   }
   
+  console.log('>>>>> inside the API');
+  console.log('>>>>> inside the API, req.query is:', req.query);
+  
    if (typeof req.query.title !== "undefined") {
     const regex = new RegExp(req.query.title, 'i')
     query.title = { $regex: regex };
