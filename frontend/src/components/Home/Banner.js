1114c0dc8cac1be6500c76dcc75a12d0c3914368
limormekaiten <limor.mekaiten@gmail.com>
2022-03-27 15:53:33 +0300

    show search box only upon click on get

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = () => {
+  const [searchVisible, setSearchVisible] = useState(false);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span>A place to get </span>
-          <form>
+          <span
+            className="get-part"
+            onClick={() => {
+              setSearchVisible(true);
+            }}
+          >
+            A place to get{" "}
+          </span>
+          <form style={{ display: searchVisible ? "inline" : "none" }}>
             <input
               type="text"
               placeholder="what is there you truly desire?"
