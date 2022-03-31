302b714d7b763dc3cdf914bb9d62107abc5fc450
Lior Brauer <liorbrauer@gmail.com>
2022-03-31 10:35:15 +0300

    dont throw on 404

--- backend/lib/event.js
+++ backend/lib/event.js
@@ -14,13 +14,12 @@ const axios = axiosLib.create({
 async function sendEvent(event, metadata, async=true) {
   const sendEventCall = async function() { 
     axios.post(`/users/${WILCO_ID}/event`, 
-      JSON.stringify({ event, metadata }),
       {
         validateStatus: function (status) {
          return status < 500; // Resolve only if the status code is less than 500
         }
       }
-    ); 
+    );
   }
 
   if (async) {
