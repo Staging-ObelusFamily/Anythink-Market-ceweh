298b808c43308f786a623726e980be5d86aedfbe
limormekaiten <limor.mekaiten@gmail.com>
2022-03-31 10:47:21 +0300

    show empty view on no results

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
+  const [searchVisible, setSearchVisible] = useState(true);
+
+  const onSearchChange = (event) => {
+    props.onSearchFilter(
+        event.target.value,
+        (page) => agent.Items.byTitle(event.target.value),
+        agent.Items.byTitle(event.target.value)
+    );
+  }
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span>A place to get </span>
-          <form>
+          <span
+            id="get-part"
+            onClick={() => {
+              setSearchVisible(true);
+            }}
+          >
+            A place to get{" "}
+          </span>
+          <form style={{ display: searchVisible ? "inline" : "none" }}>
             <input
+              style={{ width: "250px" }}
               type="text"
               placeholder="what is there you truly desire?"
               name="term"
-              className="search-box"
+              id="search-box"
+              onChange={onSearchChange}
             />
           </form>
           <span> the cool stuff.</span>
